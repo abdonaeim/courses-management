3e326eba02d3865c740ac5b7c5bac93e27b3cadb
Abdelrahman Naeim <abdelrhmannaeim7@gmail.com>
2026-01-19 22:28:11 +0200

    Update plugin version to 3.0.3

--- courses-management.php
+++ courses-management.php
@@ -3,7 +3,7 @@
  * Plugin Name: Courses Management
  * Plugin URI: https://wa.me/201020170951
  * Description: Complete course and attendance management system
- * Version: 3.0.2
+ * Version: 3.0.3
  * Author: Abdelrhman Naeim
  * Text Domain: courses-management
  * Requires at least: 5.0
@@ -15,7 +15,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Plugin Constants
-define('CM_VERSION', '3.0.0');
+define('CM_VERSION', '3.0.3');
 define('CM_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('CM_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('CM_PLUGIN_FILE', __FILE__);
